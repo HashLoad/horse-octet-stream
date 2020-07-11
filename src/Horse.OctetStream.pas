9e0216694bd2eb84736d74a291a0f954ba13879a
slipkbode <slipkbode@msn.com>
2020-07-11 08:20:34 -0400

    Alterada a classe para enviar o contentype que eu definir e se quero que sejá inline no html

--- src/Horse.OctetStream.pas
+++ src/Horse.OctetStream.pas
@@ -7,12 +7,14 @@ uses System.SysUtils, Horse, System.Classes;
 type
   TFileReturn = class
   private
-    FName: string;
+    FName  : string;
     FStream: TStream;
+    FInline: Boolean;
   public
-    property Stream: TStream read FStream write FStream;
-    property Name: string read FName write FName;
-    constructor Create(AName: string; AStream: TStream);
+    property Stream : TStream read FStream write FStream;
+    property Name   : string read FName write FName;
+    property &Inline: Boolean read FInline write FInline;
+    constructor Create(AName: string; AStream: TStream; const AInline: Boolean = False); reintroduce;
   End;
 
 procedure OctetStream(Req: THorseRequest; Res: THorseResponse; Next: TProc);
@@ -24,7 +26,7 @@ uses Web.HTTPApp, System.Math;
 procedure GetAllDataAsStream(ARequest: TWebRequest; AStream: TMemoryStream);
 var
   BytesRead, ContentLength: Integer;
-  Buffer: array [0 .. 1023] of Byte;
+  Buffer                  : array [0 .. 1023] of Byte;
 begin
   AStream.Clear;
 
@@ -32,64 +34,78 @@ begin
   ContentLength := ARequest.ContentLength;
   while ContentLength > 0 do
   begin
-    BytesRead := ARequest.ReadClient(Buffer[0],
-      Min(ContentLength, SizeOf(Buffer)));
+    BytesRead := ARequest.ReadClient(Buffer[0], Min(ContentLength, SizeOf(Buffer)));
     if BytesRead < 1 then
       Break;
     AStream.WriteBuffer(Buffer[0], BytesRead);
     Dec(ContentLength, BytesRead);
-  end;
+  End;
   AStream.Position := 0;
-end;
+End;
 
 procedure OctetStream(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 const
   CONTENT_TYPE = 'application/octet-stream';
 var
-  LWebRequest: TWebRequest;
+  LWebRequest : TWebRequest;
   LWebResponse: TWebResponse;
-  LContent: TObject;
+  LContent    : TObject;
 begin
   LWebRequest := THorseHackRequest(Req).GetWebRequest;
 
-  if (LWebRequest.MethodType in [mtPost, mtPut]) and
-    (LWebRequest.ContentType = CONTENT_TYPE) then
+  if (LWebRequest.MethodType in [mtPost, mtPut]) and (LWebRequest.ContentType = CONTENT_TYPE) then
   begin
     LContent := TMemoryStream.Create;
     GetAllDataAsStream(LWebRequest, TMemoryStream(LContent));
     THorseHackRequest(Req).SetBody(LContent);
-  end;
+  End;
 
   Next;
 
   LWebResponse := THorseHackResponse(Res).GetWebResponse;
-  LContent := THorseHackResponse(Res).GetContent;
+  LContent     := THorseHackResponse(Res).GetContent;
 
   if Assigned(LContent) and LContent.InheritsFrom(TStream) then
   begin
-    LWebResponse.ContentType := CONTENT_TYPE;
+    if LWebResponse.ContentType.Trim.IsEmpty then
+    begin
+      LWebResponse.ContentType := CONTENT_TYPE;
+    End;
+
     LWebResponse.SetCustomHeader('Content-Disposition', 'attachment');
-    LWebResponse.FreeContentStream := false;
-    LWebResponse.ContentStream := TStream(LContent);
+    LWebResponse.FreeContentStream := False;
+    LWebResponse.ContentStream     := TStream(LContent);
     LWebResponse.SendResponse;
-  end;
+  End;
 
   if Assigned(LContent) and LContent.InheritsFrom(TFileReturn) then
   begin
-    LWebResponse.ContentType := CONTENT_TYPE;
-    LWebResponse.SetCustomHeader('Content-Disposition',
-      'attachment; ' + 'filename="' + TFileReturn(LContent).Name + '"');
+    if LWebResponse.ContentType.Trim.IsEmpty then
+    begin
+      LWebResponse.ContentType := CONTENT_TYPE;
+    End;
+
+    if TFileReturn(LContent).&Inline then
+    begin
+      LWebResponse.SetCustomHeader('Content-Disposition', 'inline; ' + 'filename="' + TFileReturn(LContent).Name + '"');
+    End
+    else
+    begin
+      LWebResponse.SetCustomHeader('Content-Disposition', 'attachment; ' + 'filename="' + TFileReturn(LContent).Name + '"');
+    End;
+
     LWebResponse.ContentStream := TFileReturn(LContent).Stream;
     LWebResponse.SendResponse;
-  end;
-end;
+  End;
+End;
 
 { TFileReturn }
 
-constructor TFileReturn.Create(AName: string; AStream: TStream);
+constructor TFileReturn.Create(AName: string; AStream: TStream; const AInline: Boolean = False);
 begin
-  Name := AName;
-  Stream := AStream;
-end;
+  Name    := AName;
+  Stream  := AStream;
+  &Inline := AInline;
+End;
 
-end.
+End.
